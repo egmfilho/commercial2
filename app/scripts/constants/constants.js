91cd81d84af310db822947006ba9b267ee4976ba
egmfilho <edumig12@hotmail.com>
2017-08-02 12:04:55 -0300

    version upgrade

--- app/scripts/constants/constants.js
+++ app/scripts/constants/constants.js
@@ -2,7 +2,7 @@
 * @Author: egmfilho
 * @Date:   2017-05-29 10:49:05
 * @Last Modified by:   egmfilho
-* @Last Modified time: 2017-08-02 09:40:38
+* @Last Modified time: 2017-08-02 12:04:15
 */
 
 (function() {
@@ -11,8 +11,8 @@
 
 	angular.module('commercial2.constants')
 		.constant('Constants', {
-			'api': 'http://172.16.0.6/commercial2.api/',
-			'version': '2.0.0 rc2',
+			'api': 'http://172.16.0.82/commercial2.api/',
+			'version': '2.0.0 rc3',
 			'cookie': 'commercial.currentUser',
 			'debug': false,
 			'isElectron': window && window.process && window.process.versions['electron']
