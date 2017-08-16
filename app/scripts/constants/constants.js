2bbd8899c2a0268ba250e74e60dd3a1a0d44569a
egmfilho <edumig12@hotmail.com>
2017-08-16 18:02:56 -0300

    Version increment

--- app/scripts/constants/constants.js
+++ app/scripts/constants/constants.js
@@ -2,7 +2,7 @@
 * @Author: egmfilho
 * @Date:   2017-05-29 10:49:05
 * @Last Modified by:   egmfilho
-* @Last Modified time: 2017-08-07 14:25:23
+* @Last Modified time: 2017-08-16 14:26:26
 */
 
 (function() {
@@ -13,7 +13,7 @@
 		.constant('Constants', {
 			'api': 'http://172.16.0.82/commercial2.api/',
 			'app-name': 'Commercial - Gestor de vendas',
-			'version': '2.0.0 rc4',
+			'version': '2.0.0 rc5',
 			'cookie': 'commercial.currentUser',
 			'debug': false,
 			'isElectron': window && window.process && window.process.versions['electron'],
