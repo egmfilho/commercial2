c75d9afe0c5f84449457cedbc849787b6a00e83b
egmfilho <edumig12@hotmail.com>
2017-09-04 15:31:39 -0300

    bug fixed in order_not_doc

--- app/scripts/services/models/order.js
+++ app/scripts/services/models/order.js
@@ -280,13 +280,12 @@
 			/* Concatena a observacao do endereco nas observacoes da nota */
 			var obsFlag = Globals.get('obsFlag');
 			if (address.person_address_note) {
-				console.log(address.person_address_note);
-				// if (this.order_note_doc.indexOf(obsFlag) >= 0) {
-				// 	this.order_note_doc = this.order_note_doc.split(obsFlag)[0];
-				// }
+				if (this.order_note_doc && this.order_note_doc.indexOf(obsFlag) >= 0) {
+					this.order_note_doc = this.order_note_doc.split(obsFlag)[0];
+				}
 
-				// this.order_note_doc += obsFlag;
-				// this.order_note_doc += address.person_address_note;	
+				this.order_note_doc += obsFlag;
+				this.order_note_doc += address.person_address_note;	
 			}
 		}
 
