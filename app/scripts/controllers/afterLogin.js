65b4c194343c2378eed212cc65f8da22c5016241
egmfilho <edumig12@hotmail.com>
2017-10-19 09:00:16 -0200

    bug fixes

--- app/scripts/controllers/afterLogin.js
+++ app/scripts/controllers/afterLogin.js
@@ -55,7 +55,7 @@
 					customer: success.data.data.person_category.client_category
 				});
 
-				Globals.set('api', success.data.data.api);
+				// Globals.set('api', success.data.data.api);
 				Globals.set('contact-types', success.data.data.person_address_contact_type);
 				Globals.set('logo', success.data.data.logo);
 				Globals.set('print-message', success.data.data.order);
@@ -103,7 +103,9 @@
 			Globals.set('default-person-type', 'F');
 			Globals.set('default-icms-type', { code: 2, value: 'ISENTO' });
 			Globals.set('elective-icms-type', { code: 9 });
+
 			Globals.set('public-place-types', [ 'AV', 'EST', 'PC', 'R', 'RUA', 'ROD' ]);
+
 			Globals.set('obsFlag', '\n\nObs. de Entrega: ');
 
 			Globals.set('order-status-labels', {
