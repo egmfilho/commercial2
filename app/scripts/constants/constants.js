83ce821cc01bf31c3755a9c567cf4724546e9d48
egmfilho <edumig12@hotmail.com>
2017-10-20 16:58:59 -0200

    coding

--- app/scripts/constants/constants.js
+++ app/scripts/constants/constants.js
@@ -1,8 +1,8 @@
 /*
 * @Author: egmfilho
 * @Date:   2017-05-29 10:49:05
-* @Last Modified by:   egmfilho
-* @Last Modified time: 2017-09-11 12:00:53
+ * @Last Modified by: egmfilho
+ * @Last Modified time: 2017-10-20 16:58:51
 */
 
 (function() {
@@ -11,29 +11,29 @@
 
 	angular.module('commercial2.constants')
 		.constant('Constants', {
-			'api': [ // comentar antes de dar build
-				{
-					'id': 0, 
-					'name': 'VPN (dev)', 
-					'address': 'http://172.16.0.6/commercial2.api/' 
-				},
-				{
-					'id': 1,
-					'name': 'VPN Teste (dev)', 
-					'address': 'http://172.16.0.6/commercial2.api.teste/'
-				},
-				{
-					'id': 2, 
-					'name': 'Externo (dev)', 
-					'address': 'http://187.16.252.130/commercial2.api/' 
-				},
-				{
-					'id': 3,
-					'name': 'Externo Teste (dev)', 
-					'address': 'http://187.16.252.130/commercial2.api.teste/' 
-				}
-			],
-			// 'api': [], // descomentar antes de dar build
+			// 'api': [ // comentar antes de dar build
+			// 	{
+			// 		'id': 0, 
+			// 		'name': 'VPN (dev)', 
+			// 		'address': 'http://172.16.0.6/commercial2.api/' 
+			// 	},
+			// 	{
+			// 		'id': 1,
+			// 		'name': 'VPN Teste (dev)', 
+			// 		'address': 'http://172.16.0.6/commercial2.api.teste/'
+			// 	},
+			// 	{
+			// 		'id': 2, 
+			// 		'name': 'Externo (dev)', 
+			// 		'address': 'http://187.16.252.130/commercial2.api/' 
+			// 	},
+			// 	{
+			// 		'id': 3,
+			// 		'name': 'Externo Teste (dev)', 
+			// 		'address': 'http://187.16.252.130/commercial2.api.teste/' 
+			// 	}
+			// ],
+			'api': [], // descomentar antes de dar build
 			'app-name': 'Commercial - Gestor de vendas',
 			'version': '2.0.0 rc6',
 			'cookie': 'commercial.currentUser',
