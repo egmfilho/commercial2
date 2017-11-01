cd567f090fc8edfa403841b4a17cbe44413ba4d1
egmfilho <edumig12@hotmail.com>
2017-11-01 11:30:03 -0200

    Melhorias

--- app/scripts/constants/constants.js
+++ app/scripts/constants/constants.js
@@ -2,7 +2,7 @@
  * @Author: egmfilho <egmfilho@live.com>
  * @Date:   2017-05-29 10:49:05
  * @Last Modified by: egmfilho
- * @Last Modified time: 2017-10-24 17:39:58
+ * @Last Modified time: 2017-11-01 10:17:46
  */
 
 (function() {
@@ -16,34 +16,34 @@
 					'id': 0, 
 					'name': 'Local', 
 					'address': 'http://172.16.0.6/commercial2.api/' 
+				},
+				{
+					'id': 1, 
+					'name': 'Local Teste', 
+					'address': 'http://172.16.0.6/commercial2.api.teste/' 
+				},
+				{
+					'id': 2,
+					'name': 'Externo Altarede', 
+					'address': 'http://187.16.252.130/commercial2.api/' 
+				},
+				{
+					'id': 3,
+					'name': 'Externo NQT', 
+					'address': 'http://186.219.4.163/commercial2.api/'
+				},
+				{
+					'id': 4,
+					'name': 'Alessandro Notebook', 
+					'address': 'http://172.16.0.176/commercial2.api/'
 				}
-				// {
-				// 	'id': 1, 
-				// 	'name': 'Local Teste', 
-				// 	'address': 'http://172.16.0.6/commercial2.api.teste/' 
-				// },
-				// {
-				// 	'id': 2,
-				// 	'name': 'Externo Altarede', 
-				// 	'address': 'http://187.16.252.130/commercial2.api/' 
-				// },
-				// {
-				// 	'id': 3,
-				// 	'name': 'Externo NQT', 
-				// 	'address': 'http://186.219.4.163/commercial2.api/'
-				// },
-				// {
-				// 	'id': 4,
-				// 	'name': 'Alessandro Notebook', 
-				// 	'address': 'http://172.16.0.176/commercial2.api/'
-				// }
 			],
 			'app-name': 'Commercial - Gestor de vendas',
-			'version': '2.0.0 rc7.2',
+			'version': '2.0.0 rc7.3',
 			'cookie': 'commercial.currentUser',
 			'debug': false,
 			'isElectron': window && window.process && window.process.versions['electron'],
-			'zoomFactor': 1.15
+			'zoomFactor': 1
 		});
 
 })();
